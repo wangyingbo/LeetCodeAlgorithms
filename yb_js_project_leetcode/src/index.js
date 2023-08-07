83466b3ee7b7e6b9bb035870c4540d7b4f9bb2bc
yingbo5 <yingbo5@staff.weibo.com>
2023-08-07 19:13:55 +0800

    add 2_remove_element

--- yb_js_project_leetcode/src/index.js
+++ yb_js_project_leetcode/src/index.js
@@ -1,8 +1,9 @@
 import { start_merge_array } from './interview150/1_merge_array.js';
+import { start_remove_element } from './interview150/2_remove_element.js';
 
 
 start_merge_array();
-
+start_remove_element();
 
 
 var __main__ = function() {
