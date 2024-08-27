b083b398d578548c5ff19f2dc0d8365d1e7bcb23
yingbo5 <yingbo5@staff.weibo.com>
2024-08-27 20:17:20 +0800

    add remove_dupliates

--- yb_js_project_leetcode/src/index.js
+++ yb_js_project_leetcode/src/index.js
@@ -1,13 +1,14 @@
 import { start_merge_array } from './interview150/1_merge_array.js';
 import { start_remove_element } from './interview150/2_remove_element.js';
 import { start_remove_duplicates_from_sorted_array } from './interview150/3_remove_duplicates_from_sorted_array.js';
+import { start_remove_duplicates_from_sorted_array_2 } from './interview150/3_remove_duplicates_from_sorted_array_2.js';
 
 
 
 start_merge_array();
 start_remove_element();
 start_remove_duplicates_from_sorted_array();
-
+start_remove_duplicates_from_sorted_array_2();
 
 
 var __main__ = function() {
--- yb_js_project_leetcode/src/interview150/4_remove_duplicates_from_sorted_array_part2.js
+++ yb_js_project_leetcode/src/interview150/4_remove_duplicates_from_sorted_array_part2.js
@@ -13,12 +13,32 @@
 
 /**-------------------------------- test -------------------------------- */
 
+// 删除有序数组中的重复项（中等）
+var removeDuplicates = function(nums) {
+    let i = 0;
+    let j = 0;
+
+    while (j < nums.length) {
+        if (nums[i] === nums[j]) {
+            j++;
+        } else {
+            nums[i + 1] = nums[j];
+            i++;
+            j++;
+        }
+    }
+
+    return i + 1;
+
+};
+
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var start_remove_duplicates_from_sorted_array_2 = function(nums) {
-
+    return removeDuplicates(nums);
 };
 
 export { start_remove_duplicates_from_sorted_array_2 }
